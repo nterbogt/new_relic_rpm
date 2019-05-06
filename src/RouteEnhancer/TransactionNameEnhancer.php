95dd8d1f35f5e30f72505fddd52b053b219fec74
chrischarlton <chrischarlton@17089.no-reply.drupal.org>
2019-05-06 21:43:48 +1000

    Issue #3037240 by Chris Charlton: TransactionNameEnhancer uses RouteEnhancerInterface (deprecated in core 8.5.0)

--- src/RouteEnhancer/TransactionNameEnhancer.php
+++ src/RouteEnhancer/TransactionNameEnhancer.php
@@ -3,14 +3,14 @@
 namespace Drupal\new_relic_rpm\RouteEnhancer;
 
 use Drupal\Core\Controller\ControllerResolverInterface;
-use Drupal\Core\Routing\Enhancer\RouteEnhancerInterface;
+use Drupal\Core\Routing\EnhancerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Route;
 
 /**
  * Enhances routes with a dynamic transaction name.
  */
-class TransactionNameEnhancer implements RouteEnhancerInterface {
+class TransactionNameEnhancer implements EnhancerInterface {
 
   private $resolver;
 
@@ -24,22 +24,18 @@ class TransactionNameEnhancer implements RouteEnhancerInterface {
   /**
    * {@inheritdoc}
    */
-  public function applies(Route $route) {
-    return $route->hasDefault('_transaction_name_callback');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function enhance(array $defaults, Request $request) {
-    $cb = $defaults['_transaction_name_callback'];
-    $callable = $this->resolver->getControllerFromDefinition($cb);
-    // Clone the request so we can set the attributes now.  Otherwise,
-    // attributes aren't populated until after the route is enhanced.
-    $cloned = clone $request;
-    $cloned->attributes->replace($defaults);
-    $arguments = $this->resolver->getArguments($cloned, $callable);
-    $defaults['_transaction_name'] = call_user_func_array($callable, $arguments);
+    $route = $defaults[RouteObjectInterface::ROUTE_OBJECT];
+    if (!$route->hasDefault('_transaction_name_callback')) {
+      $cb = $defaults['_transaction_name_callback'];
+      $callable = $this->resolver->getControllerFromDefinition($cb);
+      // Clone the request so we can set the attributes now.  Otherwise,
+      // attributes aren't populated until after the route is enhanced.
+      $cloned = clone $request;
+      $cloned->attributes->replace($defaults);
+      $arguments = $this->resolver->getArguments($cloned, $callable);
+      $defaults['_transaction_name'] = call_user_func_array($callable, $arguments);
+    }
     return $defaults;
   }
 
